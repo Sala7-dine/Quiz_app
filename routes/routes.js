99b99b51bc4bfda367e88c3d0199f06d233d1798
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-30 16:07:42 +0100

    page login

--- routes/routes.js
+++ routes/routes.js
@@ -19,5 +19,8 @@ router.post('/add',async (req, res) => {
 router.get('/register', (req, res) => {
   res.render('auth/register');
 });
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
 
 module.exports = router;
